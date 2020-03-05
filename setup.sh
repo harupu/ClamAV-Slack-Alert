18e421e484f1a63920b5c0cc7301c3d372e5101b
Harupu <harupu@gmail.com>
2020-03-06 06:45:05 +0900

    chmod shell script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo -n Please input slack webook url:
 read str
 sudo mkdir -p /opt/local/share/clamav/
 sudo sh -c "cat clam_monitor.sh | sed \"s!##webook_url##!${str}!\" > /opt/local/share/clamav/clam_monitor.sh"
+sudo chmod 755 /opt/local/share/clamav/clam_monitor.sh
 sudo install -m 644 ./com.example.ClamdAlert.plist /Library/LaunchDaemons
 sudo launchctl unload /Library/LaunchDaemons/com.example.ClamdAlert.plist 2> /dev/null
 sudo launchctl load /Library/LaunchDaemons/com.example.ClamdAlert.plist
